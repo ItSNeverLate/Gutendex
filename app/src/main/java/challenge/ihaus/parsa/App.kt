e2a295f457ad46c53a3f56b186c63a004973c6c2
Mehdi <mehdiparsaeigm@gmail.com>
2021-12-03 21:32:08 +0330

    Initial Commit Makes the project structure based on MVVM Adds needed Gradle dependencies

---
+++ app/src/main/java/challenge/ihaus/parsa/App.kt
@@ -0,0 +1,5 @@
+package challenge.ihaus.parsa
+
+import android.app.Application
+
+class App:Application()
---
+++ app/src/main/java/challenge/ihaus/parsa/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package challenge.ihaus.parsa.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import challenge.ihaus.parsa.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
